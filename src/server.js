1365d2bd908621f58d8660d233c13ac9ac2104fa
lucas <lucas.scurvello@gmail.com>
2023-01-16 12:31:50 -0300

    created users.routes.js

--- src/server.js
+++ src/server.js
@@ -3,11 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.post("/users", (request, response) => {
-  const { name, email, password } = request.body;
 
-  response.json({ name, email, password });
-});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
